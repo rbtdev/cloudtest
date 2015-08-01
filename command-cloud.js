1d9fce853f554caac721698c2f9428332a3dd087
Rob Thuleen <rob.thuleen@gmail.com>
2015-07-31 22:08:28 -0700

    updated command

--- command-cloud.js
+++ command-cloud.js
@@ -11,90 +11,134 @@ program
   .option('-k, --api-key [key]', 'provider API key', env.settings.apiKey)
   .option('-r, --region [region]', 'Region', env.settings.region)
   .option('-b, --load-balancer-id [id]', 'Load Balancer ID', env.settings.loadBalancerId)
-  .parse(process.argv);
-console.log("program: " + JSON.stringify(program))
-var RS_AUTH = {
+program
+  .command('config')
+  .description('Show default config settings')
+  .action (function () {
+    console.log(JSON.stringify(env, null, 4));
+  })
+program
+  .command('servers')
+  .description('Show list of available servers')
+  .action (servers)
+program
+  .command('flavors')
+  .description('Show list of available flavors')
+  .action (flavors)
+program
+  .command('images')
+  .description('Show list of available images')
+  .action (images)
+program.parse(process.argv);
+
+function serverClient () {
+  var auth = {
     apiKey: program.apiKey, // your api key here
     username: program.username, // username as well
     region: program.region,
     provider: program.provider
+  }
+  console.log("Connecting...");
+  return pkgcloud.compute.createClient(auth);
 }
-var serverClient = pkgcloud.compute.createClient(RS_AUTH);
-var lbClient = pkgcloud.loadbalancer.createClient(RS_AUTH);
 
+function flavors () {
+  var client = serverClient();
+  console.log("Getting available flavors...");
+  client.getFlavors(function (err, flavors) {
+    var names = '-- Flavors Available --\n';
+    for (var i = flavors.length - 1; i >= 0; i--) {
+      names += flavors[i].name + '\n';
+    };
+    console.log(names);
+  });
+}
+
+function images() {
+  var client = serverClient();
+  console.log("Getting available images...");
+  client.getImages(function (err, images) {
+    var names = '-- Images Available --\n';
+    for (var i = images.length - 1; i >= 0; i--) {
+      names += images[i].name + '\n';
+    };
+    console.log(names);
+  });
+}
+
+function servers () {
+  var client = serverClient()
+  console.log("Getting available servers...");
+  client.getServers(function (err, servers) {
+    var names = '-- Servers Available --\n';
+    for (var i = servers.length - 1; i >= 0; i--) {
+      names += servers[i].name + '\n';
+    };
+    console.log(names);
+  });
+};
 
 function ensureStatus(loadBalancerId, callback) {
-    lbClient.getLoadBalancer(loadBalancerId, function(err, lb) {
-        if (err) {
-            callback(err);
-            return;
-        }
+  var auth = {
+    apiKey: program.apiKey, // your api key here
+    username: program.username, // username as well
+    region: program.region,
+    provider: program.provider
+  }
+  var lbClient = pkgcloud.loadbalancer.createClient(auth);
+  lbClient.getLoadBalancer(loadBalancerId, function(err, lb) {
+      if (err) {
+          callback(err);
+          return;
+      }
 
-        // We don't want to to do anything if we're not in a known state to begin with
-        if (lb.status !== 'ACTIVE') {
-            callback(new Error('Load Balancer status not active'));
-            return;
-        }
+      // We don't want to to do anything if we're not in a known state to begin with
+      if (lb.status !== 'ACTIVE') {
+          callback(new Error('Load Balancer status not active'));
+          return;
+      }
 
-        // check the status of each nodes. We want all of our nodes enabled
-        // before we begin rotating nodes in and out
-        if (_.any(lb.nodes, function(node) {
-            return node.condition !== 'ENABLED';
-        })) {
-            callback(new Error('All nodes must be condition:ENABLED to deploy'));
-            return;
-        }
+      // check the status of each nodes. We want all of our nodes enabled
+      // before we begin rotating nodes in and out
+      if (_.any(lb.nodes, function(node) {
+          return node.condition !== 'ENABLED';
+      })) {
+          callback(new Error('All nodes must be condition:ENABLED to deploy'));
+          return;
+      }
 
-        // If you want to any app specific validation, you could call out to that here
+      // If you want to any app specific validation, you could call out to that here
 
-        // If we meet a minimum validation, lets callback with no errors
-        callback(null, lb);
+      // If we meet a minimum validation, lets callback with no errors
+      callback(null, lb);
     });
 }
 
-serverClient.getServers(function (err, servers) {
-  debugger
-})
-serverClient.getFlavors(function (err, flavors) {
- var names = '-- Flavors Available --\n';
-  for (var i = flavors.length - 1; i >= 0; i--) {
-    names += flavors[i].name + '\n';
-  };
-  console.log(names);
-});
 
-serverClient.getImages(function (err, images) {
-  var names = '-- Images Available --\n';
-  for (var i = images.length - 1; i >= 0; i--) {
-    names += images[i].name + '\n';
-  };
-  console.log(names);
-});
-
-ensureStatus(program.loadBalancerId, function (err, lb) {
-  if (err) {
-    console.log('ERROR: ' + JSON.stringify(err))
-  }
-  else {
-    console.log('Load Balancer is ACTIVE');
-    console.log('Getting nodes...');
-    lb.getNodes(function (err, nodes) {
-      if (!err) {
-        console.log('Got Nodes.')    
-      }
-      else {
-        console.log('ERROR: ' + JSON.stringify(err));
-      }
-    });
-    lb.getStats(function (err, stats) {
-      if (!err) {
-        console.log('Got Stats: ' + JSON.stringify(stats))  
-        debugger  
-      }
-      else {
-        console.log('ERROR: ' + JSON.stringify(err));
-      }
-    });
-  }
-})
+// ensureStatus(program.loadBalancerId, function (err, lb) {
+//   if (err) {
+//     console.log('ERROR: ' + JSON.stringify(err))
+//   }
+//   else {
+//     console.log('Load Balancer is ACTIVE');
+//     console.log('Getting nodes...');
+//     lb.getNodes(function (err, nodes) {
+//       if (!err) {
+//         console.log('Got Nodes.')    
+//       }
+//       else {
+//         console.log('ERROR: ' + JSON.stringify(err));
+//       }
+//     });
+//     lb.getStats(function (err, stats) {
+//       if (!err) {
+//         console.log('Got Stats: ' + JSON.stringify(stats))  
+//         debugger  
+//       }
+//       else {
+//         console.log('ERROR: ' + JSON.stringify(err));
+//       }
+//     });
+//   }
+// })
 
